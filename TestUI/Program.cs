a8a887112ba28fcdaeb821ad278be57732cd422d
Argee Abraham <aabraham@blharbert.com>
2020-08-06 19:45:47 -0500

    first commit

---
+++ TestUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
